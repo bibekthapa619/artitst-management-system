69ced1190888fbe2e256cbbe0c23c189b3a19177
Bibek Thapa <bibek.thapa619@gmail.com>
2024-09-20 08:30:01 +0545

    Add migration for database schema

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->unique();
+            $table->date('dob');
+            $table->enum('gender',['m','f','o']);
+            $table->enum('role',['super_admin','artist_manager','artist']);
+            $table->string('address');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2024_09_20_015405_create_artists_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('super_admin_id')->constrained('users')->onDelete('cascade');
+            $table->date('first_release_year');
+            $table->integer('no_of_albums_released');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artists');
+    }
+};
