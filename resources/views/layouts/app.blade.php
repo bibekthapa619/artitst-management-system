010e17592f05b2e857da61417718080513a069a3
Bibek Thapa <bibek.thapa619@gmail.com>
2024-09-23 08:50:51 +0545

    Add flas message for success and error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,39 @@
             @include('layouts.partials.navbar')
             <div class="py-10">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    
+                    @if(session('success'))
+                        <div 
+                            x-data="{ show: true }" 
+                            x-init="setTimeout(() => show = false, 5000)" 
+                            x-show="show" 
+                            class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" 
+                            role="alert"
+                        >
+                            <strong class="font-bold">Success!</strong>
+                            <span class="block sm:inline">{{ session('success') }}</span>
+                            <span class="absolute top-0 bottom-0 right-0 px-4 py-3 cursor-pointer" @click="show = false">
+                                <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.348 5.652a1 1 0 00-1.414 0L10 8.586 7.066 5.652a1 1 0 10-1.414 1.414L8.586 10l-2.934 2.934a1 1 0 001.414 1.414L10 11.414l2.934 2.934a1 1 0 001.414-1.414L11.414 10l2.934-2.934a1 1 0 000-1.414z"/></svg>
+                            </span>
+                        </div>
+                    @endif
+
+                    @if(session('error'))
+                        <div 
+                            x-data="{ show: true }" 
+                            x-init="setTimeout(() => show = false, 5000)" 
+                            x-show="show" 
+                            class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" 
+                            role="alert"
+                        >
+                            <strong class="font-bold">Error!</strong>
+                            <span class="block sm:inline">{{ session('error') }}</span>
+                            <span class="absolute top-0 bottom-0 right-0 px-4 py-3 cursor-pointer" @click="show = false">
+                                <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.348 5.652a1 1 0 00-1.414 0L10 8.586 7.066 5.652a1 1 0 10-1.414 1.414L8.586 10l-2.934 2.934a1 1 0 001.414 1.414L10 11.414l2.934 2.934a1 1 0 001.414-1.414L11.414 10l2.934-2.934a1 1 0 000-1.414z"/></svg>
+                            </span>
+                        </div>
+                    @endif
+
                     @yield('content')
                 </div>
             </div>
